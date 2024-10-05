dfd3667b3631168f112ec9f0dcaf5bcaa777c60f
Owais Jamil <desertthunder.dev@gmail.com>
2024-10-05 10:05:21 -0500

    initial commit

---
+++ bolt.go
@@ -0,0 +1,8 @@
+// Bolt is a cli to manage dotfiles.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/desertthunder/bolt
+
+go 1.23.2
